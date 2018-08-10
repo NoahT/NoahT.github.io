a136ab6fc697bfddea85a394c87b51bf4dd9f544
Noah Teshima <noah.teshima@gmail.com>
2018-08-09 19:27:19 -0700

    added preload function for background

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-  uncoverHero();
+  preloadCheckHero();
 });
 
 function uncoverHero() {
   setTimeout(() => $('.header').addClass('header--active'),
     500);
 }
+
+function preloadCheckHero() {
+  /*preload quickfix at https://stackoverflow.com/questions/5057990/how-can-i-check-if-a-background-image-is-loaded*/
+  $('<img/>').attr('src', 'images/hero.jpg')
+  .on('load', uncoverHero)
+  .on('load', () => ($(this).remove()));
+}
+
+
